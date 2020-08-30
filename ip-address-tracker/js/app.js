5a01bf31be8fd4c6bc12db55b145df4f31dc816d
Matija Osrecki <matija.osrecki@gmail.com>
2020-08-30 14:36:17 +0200

    :construction:

--- ip-address-tracker/js/app.js
+++ ip-address-tracker/js/app.js
@@ -1,5 +1,5 @@
-
 // DOM Elements
+const header = document.querySelector('.header');
 const form = document.querySelector('.form');
 const ipInput = document.querySelector('.form__ip-input');
 const submitBtn = document.querySelector('.form__submit');
@@ -7,60 +7,36 @@ const ipEl = document.querySelector('#ip');
 const locationEl = document.querySelector('#location');
 const timezoneEl = document.querySelector('#timezone');
 const ispEl = document.querySelector('#isp');
-let myMap = L.map('map');
+const mapEl = document.querySelector('#map');
+const errorVid = document.querySelector('.error-vid');
 
 const api = {
     key: 'at_Ms5M0n5CIXhBR7UQhqNIE2agBZEoa',
     proxy: 'https://cors-anywhere.herokuapp.com/',
-    baseUrl: 'https://geo.ipify.org/api/v1',
+    base: 'https://geo.ipify.org/api/v1',
 }
 
+const mapboxToken = 'pk.eyJ1IjoibWF0MmphIiwiYSI6ImNrZWd2cmVuNjFncTAycmx0b24xMXYxdWEifQ.Q64m2Dkb1rj_hATV1NQ3gA'
+let myMap = L.map(mapEl);
 
-document.addEventListener('DOMContentLoaded', () => {
-    axios.get(`${api.proxy}https://ipapi.co/json/`)
-        .then(data => {
-            const myIp = data.data.ip;
-            console.log(data.data)
-            getIpInfo(myIp)
-        })
-
-});
-
-
-async function getIpInfo(ip = '192.212.174.101') {
-    const { proxy, baseUrl, key } = api;
+async function getIpInfo(ip = '8.8.8.8') {
+    console.log('%cLoading...', 'font-size:30px; font-weight:600; color: orange ');
+    const { proxy, base, key } = api;
     try {
-        const response = await axios.get(`${api.proxy}${baseUrl}?apiKey=${key}&ipAddress=${ip}`)
+        // const response = await axios.get(`${proxy}${base}?apiKey=${key}&ipAddress=${ip}`)
+        const response = await axios.get(`${proxy}${base}`, {
+            params: {
+                apiKey: key,
+                ipAddress: ip
+            }
+        })
         console.log(response.data);
-        displayInfo(response.data)
+        displayInfo(response.data);
     } catch (error) {
+        displayEmpty();
         console.error(error);
     }
 }
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (!ipInput) {
-        throw new Error('No address provided')
-    }
-    getIpInfo(ipInput.value);
-    console.log('submitted');
-})
-
-
-const mapboxToken = 'pk.eyJ1IjoibWF0MmphIiwiYSI6ImNrZWd2cmVuNjFncTAycmx0b24xMXYxdWEifQ.Q64m2Dkb1rj_hATV1NQ3gA'
-let lat = '51.505';
-let lon = '-0.09';
-
-myMap.setView([lat, lon], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: `${mapboxToken}`
-}).addTo(myMap);
 
 function displayMap(lat, lon) {
     myMap.setView([lat, lon], 13);
@@ -75,14 +51,72 @@ function displayMap(lat, lon) {
 }
 
 function displayInfo(data) {
+    if (!data) {
+        displayEmpty();
+        return;
+    }
+
+    header.classList.remove('error');
+    errorVid.classList.add('hide');
+    mapEl.classList.remove('hide');
+
     const { ip, isp, location } = data;
     const { city, country, region, postalCode, timezone, lat, lng } = location;
-
 
     ipEl.innerText = ip;
     locationEl.innerText = `${city}, ${country}, ${postalCode}`;
     timezoneEl.innerText = `UTC ${timezone}`
     ispEl.innerText = isp;
-    displayMap(lat, lng)
+
+    displayMap(lat, lng);
 }
+
+function displayEmpty() {
+    header.classList.add('error');
+
+    ipEl.innerText = '-';
+    locationEl.innerText = '-';
+    timezoneEl.innerText = '-'
+    ispEl.innerText = '-';
+
+    errorVid.classList.remove('hide');
+    errorVid.play();
+    mapEl.classList.add('hide');
+};
+
+function setDefault() {
+    ipEl.innerText = '1.1.1.1';
+    locationEl.innerText = `Sydney, AU, 1001`;
+    timezoneEl.innerText = `UTC +10:00`
+    ispEl.innerText = 'Cloudflare, Inc.';
+
+    let lat = '-33.8688';
+    let lon = '151.209';
+
+    myMap.setView([lat, lon], 13);
+    myMap.dragging.enable();
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: `${mapboxToken}`
+    }).addTo(myMap);
+};
+console.clear();
+setDefault();
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    if (!ipInput.value) {
+        displayEmpty();
+        throw new Error('No address provided');
+    }
+    await getIpInfo(ipInput.value);
+};
+
+form.addEventListener('submit', handleSubmit);
+
+// TODO cant move and drag on map
 
