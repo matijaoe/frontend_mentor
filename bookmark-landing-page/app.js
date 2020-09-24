b8c6c692a8aa5da676ca93c2da12314e0a6c963d
Matija Osrecki <matija.osrecki@gmail.com>
2020-09-24 15:04:22 +0200

    added email validation :sparkles: :fire:

--- bookmark-landing-page/app.js
+++ bookmark-landing-page/app.js
@@ -1,7 +1,43 @@
+const form = document.querySelector('.form');
+const emailInput = document.querySelector('.form__email');
 const questions = document.querySelectorAll('.question');
+
+
 
 questions.forEach(question => {
     question.addEventListener('click', () => {
         question.classList.toggle('opened');
     })
-})
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let email = emailInput.value;
+    if (!validateEmail(email)) {
+        showEmailError(email);
+    } else {
+        resetEmailForm()
+    }
+
+});
+
+function validateEmail(email) {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+    return emailRegex.test(email);
+}
+
+function showEmailError(email) {
+    console.log('wrong email: ', email);
+    document.querySelector('.form__state.error').classList.add('show');
+    document.querySelector('.form__state.success').classList.remove('show');
+    emailInput.classList.add('wrong');
+    emailInput.classList.remove('correct');
+}
+
+function resetEmailForm() {
+    document.querySelector('.form__state.error').classList.remove('show');
+    document.querySelector('.form__state.success').classList.add('show');
+    emailInput.classList.remove('wrong');
+    emailInput.classList.add('correct');
+    emailInput.value = '';
+}
